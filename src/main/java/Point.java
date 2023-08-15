dee32e22566f5ed920722fba63f7085a7ed6e61d
tve21 <tve21@uclive.ac.nz>
2023-08-15 12:24:41 +1200

    Test Commit

---
+++ src/main/java/Point.java
@@ -0,0 +1,67 @@
+
+class Point {
+
+
+    private int objectID;
+    private int speedLimit;
+    private int crashYear;
+
+
+    private String crashLocation1;
+    private String crashLocation2;
+    private String region;
+    private String weather;
+
+    private float longitude;
+    private float latitude;
+
+    private boolean bicycleInvolved;
+    private boolean busInvolved;
+    private boolean carInvolved;
+    private boolean holiday;
+    private boolean mopedInvolved;
+    private boolean motorcycleInvolved;
+    private boolean parkedVehicleInvolved;
+    private boolean pedestrianInvolved;
+    private boolean schoolBusInvolved;
+    private boolean trainInvolved;
+    private boolean truckInvolved;
+
+
+    public int getObjectId(){
+        return objectID;
+    }
+
+    public int getSpeedLimit(){
+        return speedLimit;
+    }
+
+    public int getCrashYear(){
+        return crashYear;
+    }
+
+    public String getCrashLocation1(){
+        return crashLocation1;
+    }
+
+    public String getCrashLocation2(){
+        return crashLocation2;
+    }
+
+    public String getRegion(){
+        return region;
+    }
+
+    public String getWeather(){
+        return weather;
+    }
+
+    public tuple
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
