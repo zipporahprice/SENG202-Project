c75845448c4196ca6e5815e4c7c06bb01637dbcb
nfa44 <nfa44@uclive.ac.nz>
2023-08-16 10:38:01 +1200

    Working database manager with initialising three tables into the SQLite local db file.

--- src/main/java/seng202/team0/repository/DatabaseManager.java
+++ src/main/java/seng202/team0/repository/DatabaseManager.java
@@ -1,12 +1,15 @@
 package seng202.team0.repository;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.io.InputStreamReader;
+import java.sql.*;
 
 public class DatabaseManager {
+
+    // TODO think about flow with jar file since the database initialiser does not work when the database exists
+    // TODO which is weird because the drop and create statements should work with or without a db file
     public static void createNewDatabase(String fileName) {
         // TODO generalise where the database is made to where the jar file is
         String url = "jdbc:sqlite:" + fileName;
@@ -16,21 +19,47 @@ public class DatabaseManager {
                 DatabaseMetaData meta = conn.getMetaData();
                 System.out.println("The driver name is " + meta.getDriverName());
                 System.out.println("A new database has been created.");
+                initialiseDatabase(conn);
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public void initialiseDatabase(Connection databaseConnection) {
+    public static void initialiseDatabase(Connection databaseConnection) {
         try {
-            InputStream in = getClass().getResourceAsStream("/sql/initialise_database.sql");
+            InputStream inputStream = DatabaseManager.class.getResourceAsStream("/sql/initialise_database.sql");
 
+            // Make sure that the input stream you get null
+            assert inputStream != null;
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            String temp;
+            StringBuilder strings = new StringBuilder();
+
+            while ((temp=bufferedReader.readLine()) != null) {
+                strings.append(temp);
+            }
+
+            String[] statements = strings.toString().split("--SPLIT");
+
+            Statement statementConnection = databaseConnection.createStatement();
+            for (String statement: statements) {
+                // TODO remove manual testing to see that strings do parse correctly
+                System.out.println(statement);
+
+                // TODO
+                statementConnection.execute(statement);
+            }
+        } catch (SQLException | IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
     public static void main(String[] args) {
         createNewDatabase("test.db");
+
     }
 
 }
