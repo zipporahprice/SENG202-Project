f33450f6b40c7314fffed4814fe02314789f1127
Zipporah Price <zpr12@uclive.ac.nz>
2023-08-23 14:15:05 +1200

    Updated FavouriteDAO with Morgan's code

--- src/main/java/seng202/team0/repository/FavouriteDAO.java
+++ src/main/java/seng202/team0/repository/FavouriteDAO.java
@@ -3,31 +3,85 @@ package seng202.team0.repository;
 import org.apache.commons.lang3.NotImplementedException;
 import seng202.team0.models.Favourite;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 /** Class that communicates with the favourites table in the database through SQL queries.
  * @author Zipporah Price
+ * @author Morgan English
  */
 public class FavouriteDAO implements DAOInterface<Favourite> {
     private final DatabaseManager databaseManager;
 
+    /**
+     * Creates a new FavouriteDAO object
+     */
     public FavouriteDAO() { this.databaseManager = DatabaseManager.getInstance(); }
 
+    /**
+     * Gets all favourite routes from the database
+     * @return List of Favourite objects
+     */
     @Override
     public List<Favourite> getAll() {
-        List<Favourite> favouriteList = new ArrayList<Favourite>();
-        return favouriteList;
+        List<Favourite> favourites = new ArrayList<Favourite>();
+        String sql = "SELECT * FROM favourites";
+        try (Connection conn = databaseManager.connect();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                favourites.add(new Favourite(
+                        rs.getInt("id"),
+                        rs.getFloat("start_lat"),
+                        rs.getFloat("start_long"),
+                        rs.getFloat("end_lat"),
+                        rs.getFloat("end_long"),
+                        rs.getString("filters")));
+            }
+            return favourites;
+        } catch (SQLException e) {
+            System.out.println(e);
+            return new ArrayList<>();
+        }
     }
 
+    /**
+     * Gets one Favourite object from the database using given ID
+     * @param id int identifier for a Favourite object
+     * @return Favourite object corresponding to id
+     */
     @Override
     public Favourite getOne(int id) {
-        throw new NotImplementedException();
+        Favourite favourite = null;
+        String sql = "SELECT * FROM favourites WHERE id=?";
+        try (Connection conn = databaseManager.connect();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, id);
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    favourite = new Favourite(
+                            rs.getInt("id"),
+                            rs.getFloat("start_lat"),
+                            rs.getFloat("start_long"),
+                            rs.getFloat("end_lat"),
+                            rs.getFloat("end_long"),
+                            rs.getString("filters"));
+                }
+                return favourite;
+            }
+        } catch (SQLException sqlException) {
+            System.out.println(sqlException);
+            return null;
+        }
     }
 
+    /**
+     * Adds a given Favourite object to a prepared statement
+     * @param ps PreparedStatement being added to
+     * @param toAdd Favourite object to be added
+     * @throws SQLException
+     */
     public void addToPreparedStatement(PreparedStatement ps, Favourite toAdd) throws SQLException {
         ps.setInt(1, toAdd.getId());
         ps.setFloat(2, toAdd.getStartLat());
@@ -37,6 +91,11 @@ public class FavouriteDAO implements DAOInterface<Favourite> {
         ps.setString(6, toAdd.getFilters());
     }
 
+    /**
+     * Adds a single given Favourite to the database
+     * @param toAdd Favourite object to be added
+     * @throws SQLException
+     */
     @Override
     public void addOne(Favourite toAdd) throws SQLException {
         String sql = "INSERT INTO crashes (id, start_lat, start_long," +
@@ -47,6 +106,11 @@ public class FavouriteDAO implements DAOInterface<Favourite> {
         ps.executeUpdate();
     }
 
+    /**
+     * Adds a given list of Favourite objects to the database
+     * @param toAdd List of Favourites to be added
+     * @throws SQLException
+     */
     public void addMultiple(List<Favourite> toAdd) throws SQLException {
         String sql = "INSERT OR IGNORE INTO crashes (id, start_lat, start_long" +
                 "end_lat, end_long, filters) values (?,?,?,?,?,?);";
@@ -60,11 +124,26 @@ public class FavouriteDAO implements DAOInterface<Favourite> {
         ps.executeBatch();
     }
 
+    /**
+     * Deletes a single Favourite object from the database using given objectId
+     * @param id ID of Favourite to be deleted
+     */
     @Override
-    public void delete(int objectId) {
-        throw new NotImplementedException();
+    public void delete(int id) {
+        String sql = "DELETE FROM sales WHERE id=?";
+        try (Connection conn = databaseManager.connect();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, id);
+            ps.executeUpdate();
+        } catch (SQLException sqlException) {
+            System.out.println(sqlException);
+        }
     }
 
+    /**
+     * Updates a given Favourite within the database
+     * @param toUpdate Favourite object to be updated
+     */
     @Override
     public void update(Favourite toUpdate) {
         throw new NotImplementedException();
