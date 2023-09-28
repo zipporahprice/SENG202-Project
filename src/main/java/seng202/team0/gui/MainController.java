d0549f5bbd63c5eaf902c2469988378bdbb07359
nfa44 <nfa44@uclive.ac.nz>
2023-09-29 08:45:27 +1300

    Refactored routing into fxml file and controller class. Need to add persistence with a singleton class for routing, add transitions back in.

--- src/main/java/seng202/team0/gui/MainController.java
+++ src/main/java/seng202/team0/gui/MainController.java
@@ -1,33 +1,22 @@
 package seng202.team0.gui;
 
-import javafx.animation.Animation;
-import javafx.animation.FadeTransition;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.concurrent.Worker;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 import netscape.javascript.JSObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import seng202.team0.business.CrashManager;
-import seng202.team0.business.FilterManager;
-import seng202.team0.models.*;
-import seng202.team0.repository.FavouriteDAO;
 
+import javafx.event.ActionEvent;
 import java.io.IOException;
-import java.sql.SQLException;
-import java.util.*;
+import java.util.Objects;
 
 /**
  * Controller for the main.fxml window
@@ -39,51 +28,14 @@ public class MainController {
 
     private static final Logger log = LogManager.getLogger(MainController.class);
     public WebView webView;
-
     @FXML
     private StackPane mainWindow;
-
-    @FXML
-    private ComboBox<String> loadRoutesComboBox;
-
-    @FXML
-    private Label numCrashesLabel;
-
-    @FXML
-    public Label ratingText;
-
-
     private Stage stage;
-
-    private GeoLocator geolocator;
     private WebEngine webEngine;
-
-    private List<Location> stops = new ArrayList<>();
-
-
-    JSObject javaScriptConnector;
-
-
-    @FXML
-    private TextField startLocation;
-
-    @FXML
-    private TextField endLocation;
-
-    @FXML
-    private TextField stopLocation;
-
+    public static JSObject javaScriptConnector;
     private MapController mapController;
-
-
-
-    @FXML
-    private AnchorPane settingsPane;
-
-
     @FXML
     private AnchorPane menuDisplayPane;
-
     private String menuPopulated = "empty";
 
 
@@ -102,22 +54,10 @@ public class MainController {
         this.stage = stage;
         stage.setMinWidth(800);
         stage.setMinHeight(600);
-        geolocator = new GeoLocator();
         stage.setMaximized(true);
         mapController = new MapController();
         mapController.setWebView(webView);
         mapController.init(stage);
-        loadRoutesComboBox.setOnAction((ActionEvent event) -> {
-            Object selectedItem = loadRoutesComboBox.getSelectionModel().getSelectedItem();
-            if (selectedItem != null) {
-                System.out.println("Selected: " + selectedItem);
-                try {
-                    loadRoute();  // Assuming this method uses the selected item
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
 
         stage.sizeToScene();
         webEngine = webView.getEngine();
@@ -171,6 +111,19 @@ public class MainController {
     /**
      * Loads filtering menu display from FXML file.
      */
+    private void loadRoutingMenuDisplay() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/routing_menu.fxml"));
+        try {
+            StackPane routingMenuDisplay = loader.load();
+            menuDisplayPane.getChildren().setAll(routingMenuDisplay);
+        } catch (IOException ioException) {
+            log.error(ioException);
+        }
+    }
+
+    /**
+     * Loads filtering menu display from FXML file.
+     */
     private void loadFilteringMenuDisplay() {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/filtering_menu.fxml"));
         try {
@@ -199,256 +152,25 @@ public class MainController {
     }
 
     /**
-     * Toggles the filtering menu display.
+     * Toggles the menu display dependent on the button clicked.
      */
-    public void toggleFiltering() {
-        if (Objects.equals(menuPopulated, "filtering")) {
+    public void toggleMenuDisplay(ActionEvent event) {
+        Button menuButton = (Button) event.getSource();
+        String menuChoice = (String) menuButton.getUserData();
+
+        if (Objects.equals(menuPopulated, menuChoice)) {
             loadEmptyMenuDisplay();
             menuPopulated = "empty";
-        } else {
+        } else if (Objects.equals("routing", menuChoice)) {
+            loadRoutingMenuDisplay();
+            menuPopulated = menuChoice;
+        } else if (Objects.equals("filtering", menuChoice)) {
             loadFilteringMenuDisplay();
-            menuPopulated = "filtering";
-        }
-    }
-
-    /**
-     * Toggles the settings menu display.
-     */
-    public void toggleSettings() {
-        if (Objects.equals(menuPopulated, "settings")) {
-            loadEmptyMenuDisplay();
-            menuPopulated = "empty";
-        } else {
+            menuPopulated = menuChoice;
+        } else if (Objects.equals("settings", menuChoice)) {
             loadSettingsMenuDisplay();
-            menuPopulated = "settings";
-        }
-    }
-
-
-    private void displayRoute(Route... routes) {
-        List<Route> routesList = new ArrayList<>();
-        Collections.addAll(routesList, routes);
-        javaScriptConnector.call("displayRoute", Route.routesToJSONArray(routesList));
-    }
-
-    /**
-     * Adds a location when the "Add Location" button is clicked.
-     * Uses Geolocator class to turn the address into a lat, lng pair
-     */
-
-    @FXML
-    private Location getStart() {
-        String address = startLocation.getText().trim();
-        if (address.isEmpty()) {
-            return null;
-        }
-        Location newMarker = geolocator.getLocation(address);
-        //javaScriptConnector.call("addMarker", address, newMarker.lat, newMarker.lng);
-        return newMarker;
-    }
-
-    @FXML
-    private Location getEnd() {
-        String address = endLocation.getText().trim();
-        if (address.isEmpty()) {
-            return null;
-        }
-        Location newMarker = geolocator.getLocation(address);
-        //javaScriptConnector.call("addMarker", address, newMarker.lat, newMarker.lng);
-        return newMarker;
-    }
-
-
-
-    @FXML
-    private void saveRoute() throws SQLException {
-        Location start = getStart();
-        Location end = getEnd();
-        String filters = FilterManager.getInstance().toString();
-        String startAddress = geolocator.getAddress(start.latitude, start.longitude);
-        String endAddress = geolocator.getAddress(end.latitude, end.longitude);
-        Favourite favourite = new Favourite(startAddress, endAddress, start.latitude, start.longitude, end.latitude, end.longitude, filters);
-        FavouriteDAO favorites = new FavouriteDAO();
-        favorites.addOne(favourite);
-    }
-
-
-    @FXML
-    private void displayRoutes() {
-        FavouriteDAO favourites = new FavouriteDAO();
-        List<Favourite> favouritesList = favourites.getAll();
-        ObservableList<String> items = FXCollections.observableArrayList(favouritesList.stream().map(favourite -> {return favourite.getStartAddress()+" to "+favourite.getEndAddress();}).toList());
-        loadRoutesComboBox.setItems(items);
-    }
-
-    @FXML
-    private void loadRoute() throws SQLException {
-        int favouriteID = loadRoutesComboBox.getSelectionModel().getSelectedIndex()+1;
-        System.out.println(favouriteID);
-        if (favouriteID != 0 && favouriteID != -1) {
-            FavouriteDAO favourites = new FavouriteDAO();
-            Favourite favourite = favourites.getOne(favouriteID);
-
-            // Update FilterManager class with the filters associated to the favourite route and Checkboxes to match
-            FilterManager filters = FilterManager.getInstance();
-            filters.updateFiltersWithQueryString(favourite.getFilters());
-
-            // Generates a route and makes sure stops is cleared
-            stops.clear();
-            generateRouteAction(favourite);
-
-            startLocation.setText(favourite.getStartAddress());
-            endLocation.setText(favourite.getEndAddress());
-        }
-    }
-
-
-    @FXML
-    private Location getStop() {
-        String address = stopLocation.getText().trim();
-        if (address.isEmpty()) {
-            return null;
-        }
-        Location newMarker = geolocator.getLocation(address);
-        //javaScriptConnector.call("addMarker", address, newMarker.lat, newMarker.lng);
-        return newMarker;
-    }
-
-
-    @FXML
-    private void addStop() throws SQLException {
-        Location stop = getStop();
-        if (stop != null) {
-            stops.add(stop);
-        }
-        generateRouteAction();
-    }
-
-    @FXML
-    private void removeStop() throws SQLException {
-        if (stops.size() >= 1) {
-            stops.remove(stops.size()-1);
-            generateRouteAction();
-        }
-    }
-
-    /**
-     * Retrieves crash points that overlap with a given route within a specified danger radius.
-     * This method retrieves crash information from a database, calculates distances between the route
-     * segments and crash points using the Haversine formula, and identifies crash points that fall
-     * within the specified danger radius of the route segments.
-     *
-     * @param route        The route for which overlapping crash points are to be identified.
-     * @param dangerRadius The radius within which crash points are considered overlapping with the route.
-     * @return A list of CrashInfo objects representing crash points that overlap with the route.
-     * @throws SQLException If an SQL-related error occurs during database query execution.
-     */
-    public List<CrashInfo> getOverlappingPoints(Route route, double dangerRadius) throws SQLException {
-        List<CrashInfo> overlappingPoints = new ArrayList<>();
-        CrashManager crashManager = new CrashManager();
-        List<CrashInfo> crashPoints = crashManager.getCrashLocations().stream().map(crash -> {
-            HashMap crash1 = (HashMap) crash;
-            double latitude = (double) crash1.get("latitude");
-            double longitude = (double) crash1.get("longitude");
-            int severity = (int) crash1.get("severity");
-            return new CrashInfo(latitude, longitude, severity);
-        }).toList();
-        for (int i = 0; i < route.route.size() - 1; i++) {
-            Location start = route.route.get(i);
-            Location end = route.route.get(i + 1);
-
-            for (CrashInfo crashPoint : crashPoints) {
-                double distToStart = haversineDistance(start, crashPoint);
-                double distToEnd = haversineDistance(end, crashPoint);
-
-                if (distToStart < dangerRadius || distToEnd < dangerRadius) {
-                    // Check if crash point is along the extended line of the segment
-                    overlappingPoints.add(crashPoint);
-                }
-            }
+            menuPopulated = menuChoice;
         }
 
-        return overlappingPoints;
-    }
-    /**
-     * Calculates the Haversine distance between two geographic coordinates using the Haversine formula.
-     * The Haversine formula is used to compute the distance between two points on the Earth's surface
-     * given their latitude and longitude coordinates.
-     *
-     * @param loc1 The first location with latitude and longitude coordinates.
-     * @param loc2 The second location with latitude and longitude coordinates.
-     * @return The Haversine distance between the two locations in meters.
-     */
-    public double haversineDistance(Location loc1, CrashInfo loc2) {
-        double R = 6371000; // Earth radius in meters
-        double dLat = Math.toRadians(loc2.lat - loc1.latitude);
-        double dLon = Math.toRadians(loc2.lng - loc1.longitude);
-
-        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
-                + Math.cos(Math.toRadians(loc1.latitude)) * Math.cos(Math.toRadians(loc2.lat))
-                * Math.sin(dLon / 2) * Math.sin(dLon / 2);
-
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-        return R * c;
-    }
-
-    @FXML
-    private void generateRouteAction() throws SQLException {
-        Location start = getStart();
-        Location end = getEnd();
-
-        if (start != null && end != null) {
-            List<Location> routeLocations = new ArrayList<>();
-            routeLocations.add(start);
-            routeLocations.addAll(stops);  // add all the stops
-            routeLocations.add(end);
-
-            Route route = new Route(List.of(routeLocations.toArray(new Location[0])));
-            List<CrashInfo> crashInfos = getOverlappingPoints(route,1000);
-            int total = ratingGenerator(crashInfos);
-            ratingText.setText("Danger: "+ total + "/5");
-            numCrashesLabel.setText("Number of crashes on route: " + crashInfos.size());
-            displayRoute(route);
-        }
-    }
-
-
-    private int ratingGenerator(List<CrashInfo> crashInfos) {
-        int total = 0;
-        for (CrashInfo crash: crashInfos) {
-            total += crash.severity;
-        }
-        if (crashInfos.size() == 0) {
-            total = (total*10) - 10;
-        } else {
-            total = (total*10 / crashInfos.size()) - 10;
-        }
-        if (total > 5) {
-            total = 5;
-        } else if (total < 0) {
-            total = 0;
-        }
-        return total;
-    }
-
-
-    private void generateRouteAction(Favourite favourite) throws SQLException {
-        Location start = new Location(favourite.getStartLat(), favourite.getStartLong());
-        Location end = new Location(favourite.getEndLat(), favourite.getEndLong());
-
-        if (start != null && end != null) {
-            List<Location> routeLocations = new ArrayList<>();
-            routeLocations.add(start);
-            routeLocations.addAll(stops);
-            routeLocations.add(end);
-
-            Route route = new Route(List.of(routeLocations.toArray(new Location[0])));
-            List<CrashInfo> crashInfos = getOverlappingPoints(route,1000);
-            int total = ratingGenerator(crashInfos);
-            displayRoute(route);
-            ratingText.setText("Danger: "+ total + "/5");
-            numCrashesLabel.setText("Number of crashes on route: " + crashInfos.size());
-        }
     }
 }
---
+++ src/main/java/seng202/team0/gui/RoutingMenuController.java
@@ -0,0 +1,282 @@
+package seng202.team0.gui;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import seng202.team0.business.CrashManager;
+import seng202.team0.business.FilterManager;
+import seng202.team0.models.*;
+import seng202.team0.repository.FavouriteDAO;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.*;
+
+public class RoutingMenuController implements Initializable {
+
+    @FXML
+    private TextField startLocation;
+    @FXML
+    private TextField endLocation;
+    @FXML
+    private TextField stopLocation;
+    @FXML
+    private Label numCrashesLabel;
+    @FXML
+    private ComboBox<String> loadRoutesComboBox;
+    @FXML
+    private Label ratingText;
+
+    private GeoLocator geolocator;
+    private List<Location> stops = new ArrayList<>();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        geolocator = new GeoLocator();
+
+        loadRoutesComboBox.setOnAction((ActionEvent event) -> {
+            Object selectedItem = loadRoutesComboBox.getSelectionModel().getSelectedItem();
+            if (selectedItem != null) {
+                System.out.println("Selected: " + selectedItem);
+                try {
+                    loadRoute();  // Assuming this method uses the selected item
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
+
+    private void displayRoute(Route... routes) {
+        List<Route> routesList = new ArrayList<>();
+        Collections.addAll(routesList, routes);
+        MainController.javaScriptConnector.call("displayRoute", Route.routesToJSONArray(routesList));
+    }
+
+    /**
+     * Adds a location when the "Add Location" button is clicked.
+     * Uses Geolocator class to turn the address into a lat, lng pair
+     */
+
+    @FXML
+    private Location getStart() {
+        String address = startLocation.getText().trim();
+        if (address.isEmpty()) {
+            return null;
+        }
+        Location newMarker = geolocator.getLocation(address);
+        //javaScriptConnector.call("addMarker", address, newMarker.lat, newMarker.lng);
+        return newMarker;
+    }
+
+    @FXML
+    private Location getEnd() {
+        String address = endLocation.getText().trim();
+        if (address.isEmpty()) {
+            return null;
+        }
+        Location newMarker = geolocator.getLocation(address);
+        //javaScriptConnector.call("addMarker", address, newMarker.lat, newMarker.lng);
+        return newMarker;
+    }
+
+
+
+    @FXML
+    private void saveRoute() throws SQLException {
+        Location start = getStart();
+        Location end = getEnd();
+        String filters = FilterManager.getInstance().toString();
+        String startAddress = geolocator.getAddress(start.latitude, start.longitude);
+        String endAddress = geolocator.getAddress(end.latitude, end.longitude);
+        Favourite favourite = new Favourite(startAddress, endAddress, start.latitude, start.longitude, end.latitude, end.longitude, filters);
+        FavouriteDAO favorites = new FavouriteDAO();
+        favorites.addOne(favourite);
+    }
+
+
+    @FXML
+    private void displayRoutes() {
+        FavouriteDAO favourites = new FavouriteDAO();
+        List<Favourite> favouritesList = favourites.getAll();
+        ObservableList<String> items = FXCollections.observableArrayList(favouritesList.stream().map(favourite -> {return favourite.getStartAddress()+" to "+favourite.getEndAddress();}).toList());
+        loadRoutesComboBox.setItems(items);
+    }
+
+    @FXML
+    private void loadRoute() throws SQLException {
+        int favouriteID = loadRoutesComboBox.getSelectionModel().getSelectedIndex()+1;
+        System.out.println(favouriteID);
+        if (favouriteID != 0 && favouriteID != -1) {
+            FavouriteDAO favourites = new FavouriteDAO();
+            Favourite favourite = favourites.getOne(favouriteID);
+
+            // Update FilterManager class with the filters associated to the favourite route and Checkboxes to match
+            FilterManager filters = FilterManager.getInstance();
+            filters.updateFiltersWithQueryString(favourite.getFilters());
+
+            // Generates a route and makes sure stops is cleared
+            stops.clear();
+            generateRouteAction(favourite);
+
+            startLocation.setText(favourite.getStartAddress());
+            endLocation.setText(favourite.getEndAddress());
+        }
+    }
+
+
+    @FXML
+    private Location getStop() {
+        String address = stopLocation.getText().trim();
+        if (address.isEmpty()) {
+            return null;
+        }
+        Location newMarker = geolocator.getLocation(address);
+        //javaScriptConnector.call("addMarker", address, newMarker.lat, newMarker.lng);
+        return newMarker;
+    }
+
+
+    @FXML
+    private void addStop() throws SQLException {
+        Location stop = getStop();
+        if (stop != null) {
+            stops.add(stop);
+        }
+        generateRouteAction();
+    }
+
+    @FXML
+    private void removeStop() throws SQLException {
+        if (stops.size() >= 1) {
+            stops.remove(stops.size()-1);
+            generateRouteAction();
+        }
+    }
+
+    /**
+     * Retrieves crash points that overlap with a given route within a specified danger radius.
+     * This method retrieves crash information from a database, calculates distances between the route
+     * segments and crash points using the Haversine formula, and identifies crash points that fall
+     * within the specified danger radius of the route segments.
+     *
+     * @param route        The route for which overlapping crash points are to be identified.
+     * @param dangerRadius The radius within which crash points are considered overlapping with the route.
+     * @return A list of CrashInfo objects representing crash points that overlap with the route.
+     * @throws SQLException If an SQL-related error occurs during database query execution.
+     */
+    public List<CrashInfo> getOverlappingPoints(Route route, double dangerRadius) throws SQLException {
+        List<CrashInfo> overlappingPoints = new ArrayList<>();
+        CrashManager crashManager = new CrashManager();
+        List<CrashInfo> crashPoints = crashManager.getCrashLocations().stream().map(crash -> {
+            HashMap crash1 = (HashMap) crash;
+            double latitude = (double) crash1.get("latitude");
+            double longitude = (double) crash1.get("longitude");
+            int severity = (int) crash1.get("severity");
+            return new CrashInfo(latitude, longitude, severity);
+        }).toList();
+        for (int i = 0; i < route.route.size() - 1; i++) {
+            Location start = route.route.get(i);
+            Location end = route.route.get(i + 1);
+
+            for (CrashInfo crashPoint : crashPoints) {
+                double distToStart = haversineDistance(start, crashPoint);
+                double distToEnd = haversineDistance(end, crashPoint);
+
+                if (distToStart < dangerRadius || distToEnd < dangerRadius) {
+                    // Check if crash point is along the extended line of the segment
+                    overlappingPoints.add(crashPoint);
+                }
+            }
+        }
+
+        return overlappingPoints;
+    }
+    /**
+     * Calculates the Haversine distance between two geographic coordinates using the Haversine formula.
+     * The Haversine formula is used to compute the distance between two points on the Earth's surface
+     * given their latitude and longitude coordinates.
+     *
+     * @param loc1 The first location with latitude and longitude coordinates.
+     * @param loc2 The second location with latitude and longitude coordinates.
+     * @return The Haversine distance between the two locations in meters.
+     */
+    public double haversineDistance(Location loc1, CrashInfo loc2) {
+        double R = 6371000; // Earth radius in meters
+        double dLat = Math.toRadians(loc2.lat - loc1.latitude);
+        double dLon = Math.toRadians(loc2.lng - loc1.longitude);
+
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+                + Math.cos(Math.toRadians(loc1.latitude)) * Math.cos(Math.toRadians(loc2.lat))
+                * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return R * c;
+    }
+
+    @FXML
+    private void generateRouteAction() throws SQLException {
+        Location start = getStart();
+        Location end = getEnd();
+
+        if (start != null && end != null) {
+            List<Location> routeLocations = new ArrayList<>();
+            routeLocations.add(start);
+            routeLocations.addAll(stops);  // add all the stops
+            routeLocations.add(end);
+
+            Route route = new Route(List.of(routeLocations.toArray(new Location[0])));
+            List<CrashInfo> crashInfos = getOverlappingPoints(route,1000);
+            int total = ratingGenerator(crashInfos);
+            ratingText.setText("Danger: "+ total + "/5");
+            numCrashesLabel.setText("Number of crashes on route: " + crashInfos.size());
+            displayRoute(route);
+        }
+    }
+
+
+    private int ratingGenerator(List<CrashInfo> crashInfos) {
+        int total = 0;
+        for (CrashInfo crash: crashInfos) {
+            total += crash.severity;
+        }
+        if (crashInfos.size() == 0) {
+            total = (total*10) - 10;
+        } else {
+            total = (total*10 / crashInfos.size()) - 10;
+        }
+        if (total > 5) {
+            total = 5;
+        } else if (total < 0) {
+            total = 0;
+        }
+        return total;
+    }
+
+
+    private void generateRouteAction(Favourite favourite) throws SQLException {
+        Location start = new Location(favourite.getStartLat(), favourite.getStartLong());
+        Location end = new Location(favourite.getEndLat(), favourite.getEndLong());
+
+        if (start != null && end != null) {
+            List<Location> routeLocations = new ArrayList<>();
+            routeLocations.add(start);
+            routeLocations.addAll(stops);
+            routeLocations.add(end);
+
+            Route route = new Route(List.of(routeLocations.toArray(new Location[0])));
+            List<CrashInfo> crashInfos = getOverlappingPoints(route,1000);
+            int total = ratingGenerator(crashInfos);
+            displayRoute(route);
+            ratingText.setText("Danger: "+ total + "/5");
+            numCrashesLabel.setText("Number of crashes on route: " + crashInfos.size());
+        }
+    }
+
+}
