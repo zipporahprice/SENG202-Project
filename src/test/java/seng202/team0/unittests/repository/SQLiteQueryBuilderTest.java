71918d72a203700dd94032971247a6650bc120e7
nfa44 <nfa44@uclive.ac.nz>
2023-10-02 16:57:33 +1300

    Removed temp test and fixed WITH test adding a space at the end of the expected query.

--- src/test/java/seng202/team0/unittests/repository/SQLiteQueryBuilderTest.java
+++ src/test/java/seng202/team0/unittests/repository/SQLiteQueryBuilderTest.java
@@ -52,7 +52,7 @@ public class SQLiteQueryBuilderTest {
         map.put("end_lat", 83.68);
         map.put("end_long", 94.92);
 
-        String expectedQuery = "WITH constants(start_lat, start_long, end_lat, end_long) AS (VALUES (53.15, 52.15, 83.68, 94.92))";
+        String expectedQuery = "WITH constants(start_lat, start_long, end_lat, end_long) AS (VALUES (53.15, 52.15, 83.68, 94.92)) ";
         String query = builder.with("constants", map).getQuery();
         Assertions.assertEquals(expectedQuery, query);
     }
@@ -112,81 +112,5 @@ public class SQLiteQueryBuilderTest {
     void testGetQuery() {
         String query = builder.getQuery();
         Assertions.assertEquals(query, "");
-    }
-
-    @Test
-    void testSomething() {
-        Location startLocation = new Location(10.0, 20.0);
-        Location endLocation = new Location(30.0, 40.0);
-
-        double start_long_rad = Math.toRadians(startLocation.longitude);
-        double start_lat_rad = Math.toRadians(startLocation.latitude);
-        double end_long_rad = Math.toRadians(endLocation.longitude);
-        double end_lat_rad = Math.toRadians(endLocation.latitude);
-
-        double start_x = Math.cos(start_lat_rad) * Math.cos(start_long_rad);
-        double start_y = Math.cos(start_lat_rad) * Math.sin(start_long_rad);
-        double start_z = Math.sin(start_lat_rad);
-
-        double end_x = Math.cos(end_lat_rad) * Math.cos(end_long_rad);
-        double end_y = Math.cos(end_lat_rad) * Math.sin(end_long_rad);
-        double end_z = Math.sin(end_lat_rad);
-
-        Map<String, Number> constantsTable = new LinkedHashMap<>();
-        constantsTable.put("start_x", start_x);
-        constantsTable.put("start_y", start_y);
-        constantsTable.put("start_z", start_z);
-        constantsTable.put("end_x", end_x);
-        constantsTable.put("end_y", end_y);
-        constantsTable.put("end_z", end_z);
-
-        double minLon = Math.min(startLocation.longitude, endLocation.longitude);
-        double maxLon = Math.max(startLocation.longitude, endLocation.longitude);
-        double minLat = Math.min(startLocation.latitude, endLocation.latitude);
-        double maxLat = Math.max(startLocation.latitude, endLocation.latitude);
-
-        // One kilometre in degrees
-        double oneKilometreInDegrees = 0.008;
-
-        String tableName = "locations";
-        String crossProductMagnitude = "SQRT(POWER((COS(RADIANS(latitude)) * SIN(RADIANS(longitude)) - "
-                + tableName + ".start_y) * (" + tableName + ".end_z - " + tableName + ".start_z) - (SIN(RADIANS(latitude)) - "
-                + tableName + ".start_z) * (" + tableName + ".end_y - " + tableName + ".start_y), 2) + POWER((SIN(RADIANS(latitude)) - "
-                + tableName + ".start_z) * (" + tableName + ".end_x - " + tableName + ".start_x) - (COS(RADIANS(latitude)) * COS(RADIANS(longitude)) - "
-                + tableName + ".start_x) * (" + tableName + ".end_z - " + tableName + ".start_z), 2) + POWER((COS(RADIANS(latitude)) * COS(RADIANS(longitude)) - "
-                + tableName + ".start_x) * (" + tableName + ".end_y - " + tableName + ".start_y) - (COS(RADIANS(latitude)) * SIN(RADIANS(longitude)) - "
-                + tableName + ".start_y) * (" + tableName + ".end_x - " + tableName + ".start_x), 2))";
-        String lineMagnitude = "SQRT(POWER(" + tableName + ".end_x - " + tableName + ".start_x, 2) + POWER("
-                + tableName + ".end_y - " + tableName + ".start_y, 2) + POWER("
-                + tableName + ".end_z - " + tableName + ".start_z, 2))";
-        String aSinTheta = "ASIN(" + crossProductMagnitude + "/" + lineMagnitude + ")";
-        String worldDistance = aSinTheta + " * 6371.0";
-
-        FilterManager filterManager = FilterManager.getInstance();
-        Location previousMin = filterManager.getViewPortMin();
-        Location previousMax = filterManager.getViewPortMax();
-
-        filterManager.setViewPortMin(new Location(minLat - oneKilometreInDegrees, minLon - oneKilometreInDegrees));
-        filterManager.setViewPortMax(new Location(maxLat + oneKilometreInDegrees, maxLon + oneKilometreInDegrees));
-
-        String filterWhere = filterManager.toString();
-
-        filterManager.setViewPortMin(previousMin);
-        filterManager.setViewPortMax(previousMax);
-
-        String select = "AVG(severity)";
-        String from = "crashes";
-        String where = filterWhere + " AND " + worldDistance;
-
-        String query = SQLiteQueryBuilder.create()
-                .with(tableName, constantsTable)
-                .select(select)
-                .from(from)
-                .where(where)
-                .getQuery();
-
-        System.out.println(query);
-
-        Assertions.assertEquals(0,0);
     }
 }
