52369d48046f2a87f4235028a18bf2f0d32d1a7d
Angelica Silva <ams361@uclive.ac.nz>
2023-10-12 22:51:23 +1300

    Removed legend, added informative tooltips, changed method newPieChart to pass in a string of the column we want. Todo: make it work for severity, transport type, year, holiday.

--- src/main/java/seng202/team0/gui/GraphController.java
+++ src/main/java/seng202/team0/gui/GraphController.java
@@ -4,25 +4,28 @@ import java.io.IOException;
 import java.net.URL;
 import java.sql.*;
 import java.util.*;
-import java.util.logging.Filter;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Side;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import seng202.team0.App;
 import seng202.team0.business.FilterManager;
-import seng202.team0.models.Favourite;
 import seng202.team0.repository.DatabaseManager;
 import seng202.team0.repository.SqliteQueryBuilder;
 
@@ -34,7 +37,7 @@ public class GraphController implements Initializable {
 
     private static final Logger log = LogManager.getLogger(App.class);
     @FXML
-    public PieChart pieChartTest;
+    public PieChart pieChartMade;
 
     private Stage stage;
 
@@ -43,36 +46,50 @@ public class GraphController implements Initializable {
 
     private Connection connection;
     private DatabaseManager databaseManager;
+    private String columnOfInterest;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        ObservableList<PieChart.Data> pieChartSQLTest = newPieChart();
 
-        pieChartTest.setData(pieChartSQLTest);
-        pieChartTest.setTitle("Test Pie Graph");
+        columnOfInterest = "weather";
+        //TODO need to account for severity, transport type, year, holiday??
+        ObservableList<PieChart.Data> pieChartSQLTestData = newPieChart(columnOfInterest);
 
+        pieChartMade.setData(pieChartSQLTestData);
+        pieChartMade.setTitle("Crashes in Aotearoa by " + columnOfInterest);
+        pieChartMade.setLegendVisible(false);
+        pieChartMade.setLabelsVisible(true);
+        pieChartMade.setLabelLineLength(16);
+
+        pieChartMade.getData().forEach(data -> {
+            String percentage = String.format("%.2f%%", (data.getPieValue() / 100));
+            String slice = data.getName();
+            Tooltip toolTipPercentRegion = new Tooltip(percentage + ", " + slice);
+            Tooltip.install(data.getNode(), toolTipPercentRegion);
+        });
 
     }
-    private ObservableList<PieChart.Data> newPieChart() {
+    private ObservableList<PieChart.Data> newPieChart(String columnOfInterest) {
         ObservableList<PieChart.Data> result = FXCollections.observableArrayList();
 
         List<HashMap<String, Object>> dbList = SqliteQueryBuilder.create()
-                .select("region, COUNT(*)")
+                .select(columnOfInterest + ", COUNT(*)")
                 .from("crashes")
-                .groupBy("region")
+                .groupBy(columnOfInterest)
                 .build();
 
         ArrayList<String> sliceNames = new ArrayList<>();
         ArrayList<Double> sliceCounts = new ArrayList<>();
 
         for (HashMap<String, Object> hash : dbList) {
-            String region = (String) hash.get("region");
+            String column = (String) hash.get(columnOfInterest);
             double count = ((Number) hash.get("COUNT(*)")).doubleValue();
 
-            sliceNames.add(region);
+            sliceNames.add(column);
             sliceCounts.add(count);
 
-            System.out.println("Region: " + region + "  Count: " + count);
+            System.out.println("Region: " + column + "  Count: " + count);
+            //TODO remove print statement
         }
 
         for (int i = 0; i < sliceNames.size(); i++) {
@@ -81,6 +98,8 @@ public class GraphController implements Initializable {
 
         return result;
     }
+
+
 
     /**
      * Initialize the window.
