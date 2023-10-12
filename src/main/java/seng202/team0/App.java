87d26cb5173ff39407657b214e029b28932a4359
tve21 <tve21@uclive.ac.nz>
2023-10-12 14:38:56 +1300

    Fixed Pipeline

--- src/main/java/seng202/team0/App.java
+++ src/main/java/seng202/team0/App.java
@@ -18,23 +18,6 @@ public class App {
      */
     public static void main(String[] args) {
         // Initialises database and checks if populated
-        DatabaseManager database = new DatabaseManager(null);
-        CrashManager manager = new CrashManager();
-        List crashes = manager.getCrashLocations();
-        if (crashes.size() == 0) {
-            try {
-                CrashCsvImporter importer = new CrashCsvImporter();
-                // TODO replace with full file
-                InputStream stream = Thread.currentThread().getContextClassLoader()
-                        .getResourceAsStream("files/crash_data_10k.csv");
-                File tempFile = File.createTempFile("tempCSV", ".csv");
-                Files.copy(stream, tempFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                manager.addAllCrashesFromFile(importer, tempFile);
-            } catch (IOException e) {
-                log.error(e);
-            }
-        }
-        // Initialises GUI
         MainWindow.main(args);
 
         DatabaseManager.getInstance().initialiseDatabase();
