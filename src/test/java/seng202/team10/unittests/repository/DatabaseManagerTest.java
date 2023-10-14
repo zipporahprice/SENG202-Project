04bd84559ba910fd07f0e584b42e3e6e8f3908c5
wth42 <wth42@uclive.ac.nz>
2023-10-15 12:06:31 +1300

    updating tests with exceptions

--- src/test/java/seng202/team10/unittests/repository/DatabaseManagerTest.java
+++ src/test/java/seng202/team10/unittests/repository/DatabaseManagerTest.java
@@ -3,13 +3,18 @@ package seng202.team10.unittests.repository;
 import java.io.File;
 import java.net.URL;
 import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import seng202.team10.exceptions.DataImportException;
 import seng202.team10.io.CrashCsvImporter;
 import seng202.team10.models.Crash;
 import seng202.team10.repository.DatabaseManager;
 import seng202.team10.repository.SqliteQueryBuilder;
+import seng202.team10.unittests.io.CrashCsvImporterTest;
 
 /**
  * Test class for DatabaseManager class.
@@ -21,6 +26,8 @@ import seng202.team10.repository.SqliteQueryBuilder;
 public class DatabaseManagerTest {
 
     private static DatabaseManager manager;
+
+    private static final Logger log = LogManager.getLogger(DatabaseManagerTest.class);
 
     /**
      * Initialise manager by getting instance.
@@ -51,25 +58,30 @@ public class DatabaseManagerTest {
      */
     @Test
     void testResetDb() {
-        // Add crashes to database
-        CrashCsvImporter importer = new CrashCsvImporter();
-        URL newUrl = Thread.currentThread().getContextClassLoader()
-                .getResource("files/random_5_crashes.csv");
-        File testFile = new File(newUrl.getPath());
-        List<Crash> crashes = importer.crashListFromFile(testFile);
-        SqliteQueryBuilder.create().insert("crashes").buildSetter(crashes);
 
-        // See if it was successful in adding the crashes to be able to see
-        // if removal is successful with reset
-        Assertions.assertTrue(SqliteQueryBuilder.create().select("*")
-                .from("crashes").buildGetter().size() > 0);
+        try {
+            // Add crashes to database
+            CrashCsvImporter importer = new CrashCsvImporter();
+            URL newUrl = Thread.currentThread().getContextClassLoader()
+                    .getResource("files/random_5_crashes.csv");
+            File testFile = new File(newUrl.getPath());
+            List<Crash> crashes = importer.crashListFromFile(testFile);
+            SqliteQueryBuilder.create().insert("crashes").buildSetter(crashes);
 
-        // Reset database
-        manager.resetDb();
+            // See if it was successful in adding the crashes to be able to see
+            // if removal is successful with reset
+            Assertions.assertTrue(SqliteQueryBuilder.create().select("*")
+                    .from("crashes").buildGetter().size() > 0);
 
-        // Look into database and make sure crashes table are empty
-        Assertions.assertTrue(SqliteQueryBuilder.create().select("*")
-                .from("crashes").buildGetter().size() == 0);
+            // Reset database
+            manager.resetDb();
+
+            // Look into database and make sure crashes table are empty
+            Assertions.assertTrue(SqliteQueryBuilder.create().select("*")
+                    .from("crashes").buildGetter().size() == 0);
+        } catch (DataImportException dataImportException) {
+            log.error(dataImportException);
+        }
     }
 
 }
