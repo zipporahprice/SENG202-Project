bcfe270e957e7d3b2c0d2b67f18d0a254eecbcf4
Zipporah Price <zpr12@uclive.ac.nz>
2023-10-16 03:48:55 +1300

    Still working on favourite step definitions. Commit so I can pull.

--- src/test/java/seng202/team10/cucumber/FavouriteStepDefinitions.java
+++ src/test/java/seng202/team10/cucumber/FavouriteStepDefinitions.java
@@ -3,24 +3,67 @@ package seng202.team10.cucumber;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import seng202.team10.business.RouteManager;
+import seng202.team10.repository.DatabaseManager;
 
 public class FavouriteStepDefinitions {
+    private String name;
+    private String startLocation;
+    private String endLocation;
 
-    String where;
+    private RouteManager routeManager = RouteManager.getInstance();
 
-    @Given("the user has a start and end location entered on the routing menu")
-    public void locations_entered() {
 
+    @Given("the user has a {string} and {string} location entered on the routing menu")
+    public void locationsEntered(String startLocation, String endLocation) {
+        // Make sure the database is reset and initialised
+        DatabaseManager databaseManager = DatabaseManager.getInstance();
+        databaseManager.resetDb();
+        databaseManager.initialiseDatabase("files/crash_data_10k.csv");
+
+        // Set location strings
+        routeManager.setStartLocation(startLocation);
+        routeManager.setEndLocation(endLocation);
+    }
+
+    @Given("there is a route saved called {string}")
+    public void favouriteExists() {
+        // Make sure the database is reset and initialised
+        DatabaseManager databaseManager = DatabaseManager.getInstance();
+        databaseManager.resetDb();
+        databaseManager.initialiseDatabase("files/crash_data_10k.csv");
 
     }
+
 
     @When("the user clicks save route and enters a unique {string}")
-    public void named_route(String name) {
+    public void namedRoute(String name) {
 
     }
 
+    @When("the user selects route {string} and clicks load route")
+    public void routeLoadAction(String name) {
+
+    }
+
+    @When("the user selects route {string} and clicks delete route")
+    public void routeDeleteAction(String name) {
+
+    }
+
+
     @Then("the {string} route is saved in the database")
-    public void route_saved(String name) {
+    public void routeSaved(String name) {
+
+    }
+
+    @Then("the location {string} in the route menu is matches the favourite in the database")
+    public void routeLoaded(String name) {
+
+    }
+
+    @Then("the route {string} is deleted from the database")
+    public void routeDeleted(String name) {
 
     }
 
