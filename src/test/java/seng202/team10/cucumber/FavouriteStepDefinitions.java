6fe0466aa71491c32871e8331118a387bedcf1b5
Zipporah Price <zpr12@uclive.ac.nz>
2023-10-16 05:20:58 +1300

    Adding functionality to the FavouriteStepDefinitions test class.

--- src/test/java/seng202/team10/cucumber/FavouriteStepDefinitions.java
+++ src/test/java/seng202/team10/cucumber/FavouriteStepDefinitions.java
@@ -3,14 +3,22 @@ package seng202.team10.cucumber;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
 import seng202.team10.business.RouteManager;
+import seng202.team10.models.Crash;
+import seng202.team10.models.Favourite;
 import seng202.team10.repository.DatabaseManager;
+import seng202.team10.repository.SqliteQueryBuilder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class FavouriteStepDefinitions {
-    private String name;
-    private String startLocation;
-    private String endLocation;
-
+    private String routeName;
+    private Favourite favourite;
+    private String start;
+    private String end;
     private RouteManager routeManager = RouteManager.getInstance();
 
 
@@ -27,44 +35,64 @@ public class FavouriteStepDefinitions {
     }
 
     @Given("there is a route saved called {string}")
-    public void favouriteExists() {
+    public void favouriteExists(String routeName) {
         // Make sure the database is reset and initialised
         DatabaseManager databaseManager = DatabaseManager.getInstance();
         databaseManager.resetDb();
         databaseManager.initialiseDatabase("files/crash_data_10k.csv");
 
+        // insert a route named into database
+        // TODO helpppp
+        favourite = new Favourite(start, end,
+                0, 0, 0,
+                0, null, "car", routeName);
+        SqliteQueryBuilder.create().insert("favourites").buildSetter(new ArrayList<>(Arrays.asList(favourite)));
     }
 
 
     @When("the user clicks save route and enters a unique {string}")
-    public void namedRoute(String name) {
-
+    public void namedRoute(String routeName) {
+        this.routeName = routeName;
     }
 
-    @When("the user selects route {string} and clicks load route")
-    public void routeLoadAction(String name) {
-
-    }
-
-    @When("the user selects route {string} and clicks delete route")
-    public void routeDeleteAction(String name) {
-
+    @When("the user selects route {string} and clicks {string} route")
+    public void routeDeleteAction(String routeName, String action) {
+       this.routeName = routeName;
     }
 
 
     @Then("the {string} route is saved in the database")
-    public void routeSaved(String name) {
-
+    public void routeSaved(String routeName) {
+        // query for route in database
+        Favourite savedRoute = (Favourite) SqliteQueryBuilder.create().select("*").from("favourites")
+                .where("route_name = \"" + routeName + "\"").buildGetter().get(0);
+        Assertions.assertEquals(savedRoute.getName(), routeName);
     }
 
-    @Then("the location {string} in the route menu is matches the favourite in the database")
-    public void routeLoaded(String name) {
 
+    @Then("the location {string} has a start location matching {string}")
+    public void routeLoaded(String route, String start) {
+        // query for route's start address in database and check if equals start
+        Favourite savedRoute = (Favourite) SqliteQueryBuilder.create().select("*").from("favourites")
+                .where("route_name = \"" + routeName + "\"").buildGetter().get(0);
+        Assertions.assertEquals(savedRoute.getName(), routeName);
     }
 
     @Then("the route {string} is deleted from the database")
-    public void routeDeleted(String name) {
+    public void routeDeleted(String route) {
+        // delete from database
+        SqliteQueryBuilder.create().delete("favourites").where("route_name = \"" + routeName + "\"").buildDeleter();
+        // query for route name and show it is not in database
+        List<?> favouriteList = SqliteQueryBuilder.create()
+                .select("*")
+                .from("favourites")
+                .where("route_name = \"" + routeName + "\"")
+                .buildGetter();
 
+        favourite = new Favourite(start, end,
+                0, 0, 0,
+                0, null, "car", routeName);
+        SqliteQueryBuilder.create().insert("favourites").buildSetter(new ArrayList<>(Arrays.asList(favourite)));
     }
 
 }
--- src/test/resources/features/Favourite.feature
+++ src/test/resources/features/Favourite.feature
@@ -1,15 +1,15 @@
 Feature: Favourite
   Scenario: User wants to save the generated route as a favourite
-    Given the user has a "start" and "end" location entered on the routing menu
-    When the user clicks save route and enters a unique "name"
-    Then the "name" route is saved in the database
+    Given the user has a "Uni-Cycle Cycleway Canterbury" and "Main Street Canterbury" location entered on the routing menu
+    When the user clicks save route and enters a unique "route1"
+    Then the "route1" route is saved in the database
 
   Scenario: User wants to load a favourite route
-    Given there is a route saved called "name"
-    When the user selects route "name" and clicks "load" route
-    Then the location "name" in the route menu is matches the favourite in the database
+    Given there is a route saved called "route2"
+    When the user selects route "route2" and clicks "load" route
+    Then the location "route2" has a start location matching "Uni-Cycle Cycleway Canterbury"
 
   Scenario: User wants to delete a favourite route
-    Given there is a route saved called "name"
-    When the user selects route "name" and clicks "delete" route
-    Then the route "name" is deleted from the database
+    Given there is a route saved called "route3"
+    When the user selects route "route3" and clicks "delete" route
+    Then the route "route3" is deleted from the database
